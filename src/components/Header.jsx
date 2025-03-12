28ba0a09965d43dc229181dbe2ca1564e9be0eec
Franco Gutierrez <francogutierre523523@gmail.com>
2025-03-12 09:51:54 -0300

    Add nav styles.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/contact">Contact</Link>
+    <nav className="flex w-full pl-3 fixed">
+      {[
+        { to: "/", label: "Home" },
+        { to: "/about", label: "About" },
+        { to: "/projects", label: "Projects" },
+        { to: "/contact", label: "Contact" },
+      ].map((item) => (
+        <Link
+          key={item.to}
+          to={item.to}
+          className="rounded-md border-black w-[100px] h-[30px] relative group overflow-clip transition-all hover:scale-105"
+        >
+          <div className="bg-sample duration-200 w-full h-full flex justify-center items-center group-click:text-indigo-200 group-hover:text-indigo-400">
+            {item.label}
+          </div>
+          <div className="transition-all duration-200 ease-out bg-indigo-600 opacity-40 absolute w-[20px] h-[60px] -top-2 -skew-x-12 -translate-x-8 group-hover:translate-x-28" />
+        </Link>
+      ))}
     </nav>
   );
 };
