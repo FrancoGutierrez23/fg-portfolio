92da6f4807ab8ddf3bae7e89ecbb1bccbcdd0913
Franco Gutierrez <francogutierre523523@gmail.com>
2025-03-12 08:51:52 -0300

    Add tailwindCSS.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to My Portfolio</h1>
+      <h1 className='text-xl text-blue-500 '>Welcome to My Portfolio</h1>
       <p>This is the Home page.</p>
     </div>
   );
